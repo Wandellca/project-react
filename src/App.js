ca651a9cfee841d78fc677200e3b6bc3e65a907c
Wandell <wandellcarvalho951108@gmail.com>
2019-03-05 10:29:25 -0300

    Desenvolvendo o styles.css, index.jsx para o componente header da aplicação e estilizando aplicação total com o styles.css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
 import './bootstrap.min.css';
-import CompanyForm from "./components/CompanyForm";
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-       <h1>Ola</h1>
-      </div>
-    );
-  }
-}
+import Header from './components/Header';
 
+const App = () =>(
+	<div className="App">
+       <h1> </h1>
+      </div>
+	);
 export default App;
