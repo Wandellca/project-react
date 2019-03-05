0d1773c9166adf0f0e57bb07dea9fb9412205637
Wandell <wandellcarvalho951108@gmail.com>
2019-03-05 00:21:06 -0300

    Iniciado modificações

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import React, { Component } from 'react';
 import './bootstrap.min.css';
-import './App.css';
 import CompanyForm from "./components/CompanyForm";
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          Empresa
-        </header> 
-        <div className="container">
-          <CompanyForm  /> 
-        </div>
-        <footer className="App-footer">
-          <h5>&copy;Todos os direitos reservados - 2019</h5>
-        </footer>
+       <h1>Ola</h1>
       </div>
     );
   }
