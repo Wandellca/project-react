db01922aae7649a72df6ec2c84b44a1c0c439dec
Wandell <wandellcarvalho951108@gmail.com>
2019-03-06 18:44:46 -0300

    Aprendendo sobre estados

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './bootstrap.min.css';
+import './styles.css'
 import Header from './components/Header';
-
+import Main from './pages/main';
 const App = () =>(
 	<div className="App">
-       <h1> </h1>
+            <h1> <Header /></h1>
+            <Main />
       </div>
 	);
 export default App;
